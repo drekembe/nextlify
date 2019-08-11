13f1888732a575335092877fd68dc6fda7440478
Miran L <miran@neworigin.io>
2019-08-11 17:19:53 +0200

    Add mobx

--- pages/papi.tsx
+++ pages/papi.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import content from '../content/home.md'
+import { useObserver, useLocalStore } from 'mobx-react-lite'
 
 interface CatPage {
   title: string;
@@ -10,19 +11,32 @@ interface CatPage {
 const Papi: NextPage<{}> = () => {
   const { html, attributes } = content
   const { title, cats } = attributes as CatPage
-  return <article>
-    <h1>{title}</h1>
-    hey papi
+  const store = useLocalStore(() => ({
+    name: '',
+    setName: (e: React.ChangeEvent<HTMLInputElement>) => {
+      store.name = e.target.value;
+    },
+    get bigName() {
+      return store.name.toUpperCase()
+    }
+  }))
+  return useObserver(() =>
+    <article>
+      <h1>{title}</h1>
+      <p>{store.bigName}</p>
+      <input onChange={store.setName} value={store.name} />
+      hey papi
     <div dangerouslySetInnerHTML={{ __html: html }} />
-    <ul>
-              { cats.map((cat, k) => (
-                  <li key={k}>
-                    <h2>{cat.name}</h2>
-                    <p>{cat.description}</p>
-                  </li>
-              ))}
-          </ul>
-  </article>
+      <ul>
+        {cats.map((cat, k) => (
+          <li key={k}>
+            <h2>{cat.name}</h2>
+            <p>{cat.description}</p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  )
 }
 
 export default Papi;
